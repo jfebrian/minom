3479ac66cf7ac8d2896bd982f39fd1b73326dc57
Joanda Febrian <febrianjoanda@gmail.com>
2021-04-28 15:16:02 +0700

    Initial commit

---
+++ Minom/Model/Constants.swift
@@ -0,0 +1,25 @@
+//
+//  Constants.swift
+//  Minom
+//
+//  Created by Joanda Febrian on 28/04/21.
+//
+
+import UIKit
+
+struct K {
+    
+}
+
+struct Color {
+    static let BackgroundSecondary = UIColor(named: "Background Secondary")!
+    static let Background = UIColor(named: "Background")!
+    static let BackgroundSubtle = UIColor(named: "Background Subtle")!
+    static let LabelGrey = UIColor(named: "Label Grey")!
+    static let LabelViridian = UIColor(named: "Label Viridian")!
+    static let LightSeaGreen = UIColor(named: "Light Sea Green")!
+    static let MiddleBlueGreen = UIColor(named: "Middle Blue Green")!
+    static let GreenSheen = UIColor(named: "Green Sheen")!
+    static let MidnightGreen = UIColor(named: "Midnight Green")!
+    static let ViridianGreen = UIColor(named: "Viridian Green")!
+}

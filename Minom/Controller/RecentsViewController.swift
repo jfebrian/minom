854da6dfe30a9cfd45d955d09199b1e77225fbca
Joanda Febrian <febrianjoanda@gmail.com>
2021-04-28 22:12:02 +0700

    dev: Add navigation bar

---
+++ Minom/Controller/RecentsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RecentsViewController.swift
+//  Minom
+//
+//  Created by Joanda Febrian on 28/04/21.
+//
+
+import UIKit
+
+class RecentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavBar()
+    }
+    
+    func setupNavBar() {
+        navigationItem.title = "Minom"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
---
+++ Minom/Controller/SettingsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.swift
+//  Minom
+//
+//  Created by Joanda Febrian on 28/04/21.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Settings"
+    }
+}
